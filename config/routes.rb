868d956a40b7d3b307aa86479b3eb0f3782a289a
matt king <miromanyth@gmail.com>
2018-01-02 15:26:19 -0500

    seperating out the views complete. #TODO split up CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  get '/services',  to: 'welcome#services'
+  get '/education', to: 'welcome#education'
+  get '/about',     to: 'welcome#about'
+  get '/contact',   to: 'welcome#contact'
 
   root 'welcome#index'
 end
