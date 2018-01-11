17e6db43555812f6c17940ff2f3aaa02b3a37250
micromanyth <miromanyth@gmail.com>
2018-01-11 12:48:24 -0500

    remove shop from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :shrooms
-  resources :categories, :path => "shop" do
-  	resources :products
-  end
   
   get 'welcome/index'
   get '/services',  to: 'welcome#services'
