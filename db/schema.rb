e7a525ed732202b93c97e3540f8bf52f219807d8
micromanyth <miromanyth@gmail.com>
2018-01-11 16:17:24 -0500

    Add Product model /w name, description, price, and image columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180105182253) do
+ActiveRecord::Schema.define(version: 20180111210812) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
+  end
 
   create_table "shrooms", force: :cascade do |t|
     t.text "name"
