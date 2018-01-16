759482eedaaec3484ba459807b052dbc994d743a
micromanyth <miromanyth@gmail.com>
2018-01-16 13:33:07 -0500

    generate controllers for Cart, Order, LineItem models and set up routes for them

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   resources :shrooms
   resources :products, :path => "shop"
+  resources :orders
+
+  get 'carts/:id' => "carts#show", as: "cart"
+  delete 'carts/:id' => "carts#destroy"
+
+  post 'line_items/:id/add' => "line_items#add_quantity", as: "line_item_add"
+  post 'line_items/:id/reduce' => "line_items#reduce_quantity", as: "line_item_reduce"
+  post 'line_items' => "line_items#create"
+  get 'line_items/:id' => "line_items#show", as: "line_item"
+  delete 'line_items/:id' => "line_items#destroy"
   
   get 'welcome/index'
   get '/education', to: 'welcome#education'
