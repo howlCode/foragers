ed8c951d92bf732f187a77c70435c95c780326fe
micromanyth <miromanyth@gmail.com>
2018-01-17 21:24:19 -0500

    clean up views, and eliminate shopping cart functionality. Time to rewrite from scratch

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,5 @@
 class Product < ApplicationRecord
 	validates :name, :description, :price, :image, presence: true
-	has_many :line_items, dependent: :destroy
 	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "shopping-cart.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
