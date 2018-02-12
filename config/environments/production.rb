35d8be7efa7c33d8ba706ccd8cf73fc85bca5024
micromanyth <miromanyth@gmail.com>
2018-02-12 16:16:26 -0500

    Remove gmail from production and instead use sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,17 +2,16 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
 
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'example.com',
-  user_name:            ENV['GMAIL_USER'],
-  password:             ENV['GMAIL_PASS'],
-  authentication:       'plain',
-  enable_starttls_auto: true  }
-  # Code is not reloaded between requests.
-  config.cache_classes = true
+
+config.action_mailer.delivery_method = :sendmail
+# Defaults to:
+# config.action_mailer.sendmail_settings = {
+#   location: '/usr/sbin/sendmail',
+#   arguments: '-i'
+# }
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
+
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
