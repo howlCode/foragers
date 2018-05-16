810e5989224b545232098d1d4659ed9e64cc7249
micromanyth <miromanyth@gmail.com>
2018-05-16 14:33:49 -0400

    Add Order model unit test. All tests green

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class OrderTest < ActiveSupport::TestCase
 
   def setup
-    @order = orders(:one)
+    @order = Order.new(user_id: users(:valid).id, address_id: addresses(:one).id,
+    									 credit_card_id: credit_cards(:one).id, status: 'unsubmitted')
   end
 
   test 'valid order' do
